53a71f9989fc6df4e7e22de280a576fae3ef16c1
encobrain <encobrain@gmail.com>
2016-11-16 22:28:55 +0200

    Added more tests

--- tests/01-simple.js
+++ tests/01-simple.js
@@ -50,7 +50,7 @@ module.exports = {
 
         gotPacket = function (buf) {
             clearTimeout(timeout);
-            test.ok(buf.length == 3, 'Buf length = 3');
+            test.ok(buf.length == sendBuf.length, 'Buf length ok');
             test.deepEqual(buf, sendBuf, 'buf data  == sendBuf data');
             socket.destroy();
             test.done();
@@ -151,6 +151,147 @@ module.exports = {
         }, 1000);
 
         socket.write(sendBuf);
+    },
+
+    'Send few packets': function (test) {
+        var socket = new net.Socket(),
+            stream = new Stream(socket, streamOptions),
+
+            sendBuf1 = new Buffer([3, 45,63,34,5,345,]),
+            sendBuf2 = new Buffer([32,2,42,34]),
+            checkBuf = sendBuf1;
+            ;
+
+        gotPacket = function (buf) {
+
+            test.ok(buf.length == checkBuf.length, 'Buf length = 3');
+            test.deepEqual(buf, checkBuf, 'buf data  == sendBuf data');
+
+            if (checkBuf != sendBuf2) {
+                checkBuf = sendBuf2;
+                return;
+            }
+
+            clearTimeout(timeout);
+
+            socket.destroy();
+            test.done();
+        };
+
+        gotError = function (err) {
+            clearTimeout(timeout);
+            test.ok(false, err);
+            socket.destroy();
+            test.done();
+        };
+
+        socket.connect(PORT);
+
+        var timeout = setTimeout(function (){
+            test.ok(false, 'Packet lost');
+            socket.destroy();
+            test.done();
+        }, 1000);
+
+        stream.write(sendBuf1);
+        stream.write(sendBuf2);
+    },
+
+    'Send random packets': function (test) {
+        var socket = new net.Socket(),
+            stream = new Stream(socket, streamOptions),
+
+            sendBufs = [
+                new Buffer([3, 45,63,34,5,345,]),
+                new Buffer([43,374,45,45,32,3,54]),
+                new Buffer([8,9,67,6,7,9,8,8,9,78]),
+                new Buffer([68,4,8,4,32,46,78,27,8])
+            ],
+
+            count = 0
+        ;
+
+        gotPacket = function (buf) {
+            count++;
+
+            function received (sendedBuf) {
+                var l = sendedBuf.length;
+
+                if (l != buf.length) return false;
+
+                while (l--) if (sendedBuf[l] !== buf[l]) return false;
+
+                return true;
+            }
+
+            if (!sendBufs.some(received)) test.ok(false, 'Received error buf');
+
+            if (count > 4) test.ok(false, 'Received > 4 packets');
+        };
+
+        gotError = function (err) {
+            clearTimeout(timeout);
+            test.ok(false, err);
+            socket.destroy();
+            test.done();
+        };
+
+        socket.connect(PORT);
+
+        function send (buf) {
+            setTimeout(function () {
+                stream.write(buf);
+            }, Math.random() * 700 + 200);
+        }
+
+        sendBufs.forEach(send);
+
+        var timeout = setTimeout(function (){
+            test.ok(false, 'Packet lost');
+            socket.destroy();
+            test.done();
+        }, 2000);
+
+        setTimeout(function(){
+            clearTimeout(timeout);
+            test.ok(count === sendBufs.length, 'Received correct count of packets');
+            socket.destroy();
+            test.done();
+        },1000);
+    },
+
+    'Send packet by parts': function (test) {
+        var socket = new net.Socket(),
+
+            sendBuf = new Buffer([10 + 128, 2,3,4,5,6,7,9,5,3,4]),
+            shouldBeBuf = sendBuf.slice(1)
+            ;
+
+        gotPacket = function (buf) {
+            clearTimeout(timeout);
+            test.ok(buf.length == shouldBeBuf.length, 'Buf length ok');
+            test.deepEqual(buf, shouldBeBuf, 'buf data  == sendBuf data');
+            socket.destroy();
+            test.done();
+        };
+
+        gotError = function (err) {
+            clearTimeout(timeout);
+            test.ok(err.code === 'PACKET_LENGTH_BIG', err);
+            socket.destroy();
+            test.done();
+        };
+
+        socket.connect(PORT);
+
+        var timeout = setTimeout(function (){
+            test.ok(false, 'Packet lost');
+            socket.destroy();
+            test.done();
+        }, 1000);
+
+        for (var i = 0; i < sendBuf.length; i++)
+            setTimeout(function (){ socket.write(sendBuf.slice(this,this+1))}.bind(i), 100 + i * 50);
     },
 
     'Close server': function (test) {
